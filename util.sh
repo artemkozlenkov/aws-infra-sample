5c42671fdc89772e367488efb96849b4695a5320
Artem Kozlenkov <artem.kozlenkov@go-e.co>
2021-12-12 17:41:16 +0100

    edit util.sh

--- util.sh
+++ util.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 
 # use as `./util.sh 'commit msg'`
-git commit -m "$@";git commit --amend --reuse-message=HEAD;
+git commit -m "$@"; git commit --amend --reuse-message=HEAD;
+
+#git reset $(git commit-tree HEAD^{tree} -m $2);
